a57f882deb0a28a862fb35e704cd94ff45a96abf
Chang He <changhe3@illinois.edu>
2020-10-03 21:48:18 +0800

    First commit

---
+++ data.lua
@@ -0,0 +1,8 @@
+data.raw.recipe["miniloader"].ingredients = {{"transport-belt-loader", 1}}
+data.raw.recipe["filter-miniloader"].ingredients = {{"transport-belt-loader", 1}, {"copper-cable", 4}}
+
+data.raw.recipe["fast-miniloader"].ingredients = {{"fast-transport-belt-loader", 1}}
+data.raw.recipe["fast-filter-miniloader"].ingredients = {{"fast-transport-belt-loader", 1}, {"electronic-circuit", 4}}
+
+data.raw.recipe["express-miniloader"].ingredients = {{"express-transport-belt-loader", 1}}
+data.raw.recipe["express-filter-miniloader"].ingredients = {{"express-transport-belt-loader", 1}, {"electronic-circuit", 4}}
