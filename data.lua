3170a5ceeb425b27771d7614a7c8e8becbdbaf6d
Chang He <changhe3@illinois.edu>
2020-10-04 16:21:21 +0800

    fix category

--- data.lua
+++ data.lua
@@ -4,5 +4,7 @@ data.raw.recipe["filter-miniloader"].ingredients = {{"iron-plate", 5}, {"transpo
 data.raw.recipe["fast-miniloader"].ingredients = {{"iron-gear-wheel", 20}, {"miniloader", 1}}
 data.raw.recipe["fast-filter-miniloader"].ingredients = {{"iron-gear-wheel", 20}, {"miniloader", 1}, {"electronic-circuit", 4}}
 
-data.raw.recipe["express-miniloader"].ingredients = {{"iron-gear-wheel", 40}, {"fast-miniloader", 1}, {"lubricant", 20}}
-data.raw.recipe["express-filter-miniloader"].ingredients = {{"iron-gear-wheel", 40}, {"fast-miniloader", 1}, {"lubricant", 20}, {"electronic-circuit", 4}}
+data.raw.recipe["express-miniloader"].category = "crafting-with-fluid"
+data.raw.recipe["express-miniloader"].ingredients = {{"iron-gear-wheel", 40}, {"fast-miniloader", 1}, {name = "lubricant", type = "fluid", amount = 20}}
+data.raw.recipe["express-filter-miniloader"].category = "crafting-with-fluid"
+data.raw.recipe["express-filter-miniloader"].ingredients = {{"iron-gear-wheel", 40}, {"fast-miniloader", 1}, {name = "lubricant", type = "fluid", amount = 20}, {"electronic-circuit", 4}}
