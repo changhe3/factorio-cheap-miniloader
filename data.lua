83c4b57c05254419c28c11615b6c86e932d94ca9
Chang He <changhe3@illinois.edu>
2020-10-04 15:45:57 +0800

    Remove dependency on deadlock loaders bump to 0.2.0

--- data.lua
+++ data.lua
@@ -1,8 +1,8 @@
-data.raw.recipe["miniloader"].ingredients = {{"transport-belt-loader", 1}}
-data.raw.recipe["filter-miniloader"].ingredients = {{"transport-belt-loader", 1}, {"copper-cable", 4}}
+data.raw.recipe["miniloader"].ingredients = {{"iron-plate", 5}, {"transport-belt", 1}}
+data.raw.recipe["filter-miniloader"].ingredients = {{"iron-plate", 5}, {"transport-belt", 1}, {"copper-cable", 4}}
 
-data.raw.recipe["fast-miniloader"].ingredients = {{"fast-transport-belt-loader", 1}}
-data.raw.recipe["fast-filter-miniloader"].ingredients = {{"fast-transport-belt-loader", 1}, {"electronic-circuit", 4}}
+data.raw.recipe["fast-miniloader"].ingredients = {{"iron-gear-wheel", 20}, {"miniloader", 1}}
+data.raw.recipe["fast-filter-miniloader"].ingredients = {{"iron-gear-wheel", 20}, {"miniloader", 1}, {"electronic-circuit", 4}}
 
-data.raw.recipe["express-miniloader"].ingredients = {{"express-transport-belt-loader", 1}}
-data.raw.recipe["express-filter-miniloader"].ingredients = {{"express-transport-belt-loader", 1}, {"electronic-circuit", 4}}
+data.raw.recipe["express-miniloader"].ingredients = {{"iron-gear-wheel", 40}, {"fast-miniloader", 1}, {"lubricant", 20}}
+data.raw.recipe["express-filter-miniloader"].ingredients = {{"iron-gear-wheel", 40}, {"fast-miniloader", 1}, {"lubricant", 20}, {"electronic-circuit", 4}}
